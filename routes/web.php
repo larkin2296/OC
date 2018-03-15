51dc02dc2deb7bc8db9a0f6a27ad98d3fdba66d3
ltstudy <ltstudy@sina.com>
2018-03-15 23:43:10 +0800

    3.15lt112

--- routes/web.php
+++ routes/web.php
@@ -29,33 +29,23 @@
 
 /*网站中间键*/
 $router->group(['middleware' => "web"], function($router) {
-	/*设置语言*/
-	$router->get('language/{language}', [
-		'uses' => "LanguageController@set",
-		'as' => 'language'
-	]);
-<<<<<<< HEAD
-	$router->group(['prefix' => 'admin', 'as' => 'admin.','middleware' => ['auth']], function($router) {
-	    /*默认首页*/
-	    require(__DIR__.'/oc_routes/dash.php');
+    /*设置语言*/
+    $router->get('language/{language}', [
+        'uses' => "LanguageController@set",
+        'as' => 'language'
+    ]);
+
+    $router->group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth']], function ($router) {
+        /*默认首页*/
+        require(__DIR__ . '/oc_routes/dash.php');
         /*采购商*/
         require(__DIR__ . '/oc_routes/management/route.php');
-=======
-	// 'middleware' => ['auth'] 关闭auth lt测试
-	$router->group(['prefix' => 'admin', 'as' => 'admin.','middleware' => ['auth']], function($router) {
-	    /*默认首页*/
-	    require(__DIR__.'/routes/dash.php');
-	    /*用户信息*/
-		require(__DIR__ . '/routes/registered/route.php');
-        /*油卡管理*/
-        require(__DIR__ . '/routes/management/route.php');
->>>>>>> daab3c90c5bd55c22d3d2437ec68cbb8ec77370e
 
 
     });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
