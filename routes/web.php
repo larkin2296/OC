ca3ff7668da337c6acb9117b76261fa4a89c74f0
larkin2296 <546462977@qq.com>
2018-03-18 14:47:43 +0800

    又莫名其妙的推一次

--- routes/web.php
+++ routes/web.php
@@ -45,7 +45,7 @@ $router->group(['middleware' => "web"], function($router) {
 
     Auth::routes();
 
-<<<<<<< HEAD
+
 
     Route::get('/home', 'HomeController@index')->name('home');
     Route::get('/new', function () {
@@ -55,18 +55,3 @@ $router->group(['middleware' => "web"], function($router) {
         return view('themes/metronic/ocback/user/show');
     });
 });
-=======
-
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/new', function(){
-    return view('new');
-});
-Route::get('/show',function() {
-    return view('themes/metronic/ocback/user/show');
-});
-//Route::get('/purchasing',function(){
-//    return view('themes/metronic/ocback/backstage/purchasing/left_meau');
-//});
-
-});
->>>>>>> 48a426f38bf97189926dc451fcb9c6b593b55185
